91f83fd3a089a607b99ba37750d5952aaed0ff03
Carl di Ortus <reklamukibiras@gmail.com>
2014-03-31 19:45:38 +0300

    get_file_list returns only text files

--- filestat.py
+++ filestat.py
@@ -6,19 +6,25 @@
 # Available in MIT license (see LICENCE)
 
 
+import mimetypes
 import os
 
 
 def get_file_list(directory):
     """Returns a list of file paths in a given directory.
-        Hidden directories and hidden files are ommited."""
+        Hidden directories and hidden files are ommited.
+        File is accepted if its mimetype is None or text."""
     
     filelist = []
     
     for root, dirs, files in os.walk(directory):
         dirs[:] = [d for d in dirs if not d[0] == '.']
         for filename in files:
-            if not filename.startswith('.'):
+            if (not filename.startswith('.') and 
+                    mimetypes.guess_type(filename)[0] is None):
+                filelist.append(os.path.join(root, filename))
+            elif (not filename.startswith('.') and
+                    mimetypes.guess_type(filename)[0].startswith('text')):
                 filelist.append(os.path.join(root, filename))
     
     return filelist
